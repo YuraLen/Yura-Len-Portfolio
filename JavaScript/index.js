128033751d0a561dbfa2793da9c1a4c991e8a387
YuraLen <92933581+YuraLen@users.noreply.github.com>
2022-04-21 11:49:55 +0300

    portfolio + testimonials

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,6 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const ml_section = document.querySelector(".milestones")
     const ml_counters = document.querySelectorAll(".number span")
 
+    const prt_section = document.querySelector(".portfolio")
+    const zoom_icons = document.querySelectorAll(".zoom-icon")
+    const modal_overlay = document.querySelector(".modal-overlay")
+    const images = document.querySelectorAll(".images img")
+    const prev_btn = document.querySelector(".prev-btn")
+    const next_btn = document.querySelector(".next-btn")
+
 
     // ------- Sticky Navbar -------
 
@@ -101,4 +108,68 @@ document.addEventListener("DOMContentLoaded", () => {
         mlCounter()
 
     })
+
+    // ------- Portfolio Filter -------
+
+    let mixer = mixitup(".portfolio-gallery", {
+        selectors: {
+            target: ".prt-card"
+        },
+        animation: {
+            duration: 500,
+        },
+    })
+
+    // ------- Modal Pop up Portfolio -------
+
+    let currentIndex = 0
+
+    zoom_icons.forEach((icn, i) => icn.addEventListener("click", () => {
+        prt_section.classList.add("open");
+        document.body.classList.add("stopScrolling");
+        currentIndex = i;
+        changeImage(currentIndex)
+    }))
+
+    modal_overlay.addEventListener("click", () => {
+        prt_section.classList.remove("open");
+        document.body.classList.remove("stopScrolling");
+    })
+
+    prev_btn.addEventListener("click", () => {
+        if (currentIndex === 0) {
+            currentIndex = 5
+        } else {
+            currentIndex--
+            changeImage(currentIndex)
+        }
+    })
+
+    next_btn.addEventListener("click", () => {
+        if (currentIndex === 5) {
+            currentIndex = 0
+        } else {
+            currentIndex++
+            changeImage(currentIndex);
+        }
+    })
+
+    function changeImage(index) {
+        images.forEach(img => img.classList.remove("showImage"))
+        images[index].classList.add("showImage")
+    }
+
+    // ------- Swiper Pagination -------
+
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        speed: 500,
+        autoplay: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
 })
